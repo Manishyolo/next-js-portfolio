ca483d9561edfd8c358f80b70335b67beed15202
Manish kumar <manish04kumarsingh@gmail.com>
2026-06-17 15:12:46 +0530

    Day - 02

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import TextReveal from "./components/TextReveal";
 
 export default function Home() {
   return (
     <div className="w-full h-[200vh]">
-
-      <h1>dawdkjwaddjkj</h1>
+        <TextReveal splitBy="words" className="text-[5rem]">
+                Hello Everyone
+        </TextReveal>
+      
     </div>
   );
 }
